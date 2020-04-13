6552b5c4436d83bf651dac6c189f1768936d8510
TerryFung <singtaifung@Singde-AppleII.local>
2020-04-13 11:42:15 +0800

    first commit

---
+++ app/src/main/java/com/terryyessfung/whatsins/Activities/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.terryyessfung.whatsins.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.terryyessfung.whatsins.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
