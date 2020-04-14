9342e33c3ba573878aaaf3899adc93212f3e2e5b
TerryFunggg <terryyessfung@gmail.com>
2020-04-15 01:05:27 +0800

    update HomeFragment

--- app/src/main/java/com/terryyessfung/whatsins/Fragments/HomeFragment.java
+++ app/src/main/java/com/terryyessfung/whatsins/Fragments/HomeFragment.java
@@ -8,10 +8,22 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
+import com.terryyessfung.whatsins.Adapters.HomeRecyclerAdapter;
+import com.terryyessfung.whatsins.Model.Row;
 import com.terryyessfung.whatsins.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeFragment extends Fragment {
+    private RecyclerView staggeredRvView;
+    private HomeRecyclerAdapter mHomeRecyclerAdapter;
+    private StaggeredGridLayoutManager mStaggeredGridLayoutManager;
+
     public HomeFragment(){
 
     }
@@ -31,7 +43,33 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        super.onCreateView(inflater, container, savedInstanceState);
+
+        staggeredRvView = getView().findViewById(R.id.recyclerView_home);
         return inflater.inflate(R.layout.fragment_home,container,false);
+    }
+
+    public void fetchPost(){
+        List<Row> list = new ArrayList<>();
+        list.add(new Row(R.drawable.aa));
+        list.add(new Row(R.drawable.ee));
+        list.add(new Row(R.drawable.ff));
+        list.add(new Row(R.drawable.gg));
+        list.add(new Row(R.drawable.bb));
+        list.add(new Row(R.drawable.cc));
+        list.add(new Row(R.drawable.dd));
+        list.add(new Row(R.drawable.hh));
+        list.add(new Row(R.drawable.aa));
+        list.add(new Row(R.drawable.ee));
+        list.add(new Row(R.drawable.ff));
+        list.add(new Row(R.drawable.gg));
+        list.add(new Row(R.drawable.bb));
+        list.add(new Row(R.drawable.cc));
+        list.add(new Row(R.drawable.dd));
+        list.add(new Row(R.drawable.hh));
+
+        //mStaggeredGridLayoutManager = new StaggeredGridLayoutManager(1,StaggeredGridLayoutManager.VERTICAL);
+        staggeredRvView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mHomeRecyclerAdapter = new HomeRecyclerAdapter(getContext(),list);
+        staggeredRvView.setAdapter(mHomeRecyclerAdapter);
     }
 }
