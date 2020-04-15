c2a3ec51ed2ac54ebccbfb72198aaa71d077b30b
TerryFunggg <terryyessfung@gmail.com>
2020-04-15 23:46:04 +0800

    done refresh-recycerView basic

--- app/src/main/java/com/terryyessfung/whatsins/Fragments/HomeFragment.java
+++ app/src/main/java/com/terryyessfung/whatsins/Fragments/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.terryyessfung.whatsins.Fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,17 +12,19 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.terryyessfung.whatsins.Adapters.HomeRecyclerAdapter;
 import com.terryyessfung.whatsins.Model.Row;
 import com.terryyessfung.whatsins.R;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class HomeFragment extends Fragment {
-    private RecyclerView staggeredRvView;
+    private SwipeRefreshLayout mRefreshLayout;
+    private RecyclerView mRecyclerView;
     private HomeRecyclerAdapter mHomeRecyclerAdapter;
     private StaggeredGridLayoutManager mStaggeredGridLayoutManager;
     List<Row> list;
@@ -41,40 +44,71 @@ public class HomeFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         list = new ArrayList<>();
         list.add(new Row(R.drawable.aa));
-        list.add(new Row(R.drawable.ee));
-        list.add(new Row(R.drawable.ff));
-        list.add(new Row(R.drawable.gg));
-        list.add(new Row(R.drawable.bb));
-        list.add(new Row(R.drawable.cc));
-        list.add(new Row(R.drawable.dd));
-        list.add(new Row(R.drawable.hh));
         list.add(new Row(R.drawable.aa));
-        list.add(new Row(R.drawable.ee));
-        list.add(new Row(R.drawable.ff));
-        list.add(new Row(R.drawable.gg));
-        list.add(new Row(R.drawable.bb));
-        list.add(new Row(R.drawable.cc));
-        list.add(new Row(R.drawable.dd));
-        list.add(new Row(R.drawable.hh));
+        list.add(new Row(R.drawable.aa));
+        list.add(new Row(R.drawable.aa));
+        list.add(new Row(R.drawable.aa));
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container,false);
-        staggeredRvView = view.findViewById(R.id.recyclerView_home);
+        // refresh Layout
+        mRefreshLayout = view.findViewById(R.id.refreshLayout_home);
+        mRefreshLayout.setOnRefreshListener(swipeOnRefresh);
 
-        fetchPost();
+        //Setting recycler view
+        mRecyclerView = view.findViewById(R.id.recyclerView_home);
+        // mStaggeredGridLayoutManager = new StaggeredGridLayoutManager(1,StaggeredGridLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mHomeRecyclerAdapter = new HomeRecyclerAdapter(getContext(),list);
+        mRecyclerView.setAdapter(mHomeRecyclerAdapter);
+        testFetchPost();
         return view;
     }
 
+    // TODO Fetch the data from server
     public void fetchPost(){
-       // mStaggeredGridLayoutManager = new StaggeredGridLayoutManager(1,StaggeredGridLayoutManager.VERTICAL);
-        staggeredRvView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        mHomeRecyclerAdapter = new HomeRecyclerAdapter(getContext(),list);
-        staggeredRvView.setAdapter(mHomeRecyclerAdapter);
+
+        mHomeRecyclerAdapter.notifyDataSetChanged();
+    }
+    SwipeRefreshLayout.OnRefreshListener swipeOnRefresh = new SwipeRefreshLayout.OnRefreshListener() {
+        @Override
+        public void onRefresh() {
+            mRefreshLayout.setRefreshing(true);
+            testFetchPost();
+            mRefreshLayout.setRefreshing(false);
+        }
+    };
+
+    public void testFetchPost(){
+        Row[] rows = {
+                new Row(R.drawable.aa),
+                new Row(R.drawable.ee),
+                new Row(R.drawable.ff),
+                new Row(R.drawable.gg),
+                new Row(R.drawable.bb),
+                new Row(R.drawable.cc),
+                new Row(R.drawable.dd),
+                new Row(R.drawable.hh),
+                new Row(R.drawable.aa),
+                new Row(R.drawable.ee),
+                new Row(R.drawable.ff),
+                new Row(R.drawable.gg),
+                new Row(R.drawable.bb),
+                new Row(R.drawable.cc),
+                new Row(R.drawable.dd),
+                new Row(R.drawable.hh),
+        };
+        Random random = new Random();
+        int r = random.nextInt(rows.length);
+        Log.d("random",r+"");
+        list.add(0,rows[r]);
+        mHomeRecyclerAdapter.notifyDataSetChanged();
+
+       // mHomeRecyclerAdapter.notifyItemRangeInserted(1,5);
     }
 }
