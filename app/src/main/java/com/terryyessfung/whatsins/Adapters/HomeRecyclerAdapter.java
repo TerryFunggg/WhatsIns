e5dd8af113f8a560ba4e08211071d7f939d80b08
TerryFunggg <terryyessfung@gmail.com>
2020-04-15 17:51:20 +0800

    done like click anim

--- app/src/main/java/com/terryyessfung/whatsins/Adapters/HomeRecyclerAdapter.java
+++ app/src/main/java/com/terryyessfung/whatsins/Adapters/HomeRecyclerAdapter.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
@@ -43,9 +46,20 @@ public class HomeRecyclerAdapter extends RecyclerView.Adapter<HomeRecyclerAdapte
 
     public class ViewHolder extends RecyclerView.ViewHolder{
         ImageView img;
+        CheckBox like;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             img = itemView.findViewById(R.id.row_postImg);
+            like = itemView.findViewById(R.id.row_fav);
+            like.setOnClickListener(likeOnClick);
         }
+
+        View.OnClickListener likeOnClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Animation animation = AnimationUtils.loadAnimation(v.getContext(),R.anim.like_amin);
+                v.startAnimation(animation);
+            }
+        };
     }
 }
